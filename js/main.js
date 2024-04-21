8d9a61790bba07ea4c8580bf9ff3e4f1bf2ff123
Stanislav <162728722+KozloveStanislave@users.noreply.github.com>
2024-04-22 02:41:52 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('.fifth-block-divs, .seventh-block-divs').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
+});
+
+
+
+
+// $(function(){
+//   $('.bike-slider, .slider__items').slick({
+//       arrows: false,
+//       dots: true,
+//       fade: true,
+//       autoplay: true,
+//       autoplaySpeed: 2000,
+//   });
+//   $(".menu, .bike__column").on("click","a", function (event) {
+//   event.preventDefault();
+//   var id  = $(this).attr('href'),
+//     top = $(id).offset().top;
+//   $('body,html').animate({scrollTop: top}, 1000);
+// });
+// });
